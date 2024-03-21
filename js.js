79a0df3fceda31d0a743c01c6839f2a216341ebd
Nim06 <152057416+Nim06@users.noreply.github.com>
2024-03-22 00:35:51 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+//document.getElementById("h").textContent= `guh`;
+//document.getElementById("p").textContent= `guh`;
+
+//let x = 10;
+//console.log(`abc ${x}`);
+
+//for strings use '' or "" for assigning
+//typeof gives type of var
+// true or false is boolean
+
+let x = 10;
+let y = 'fruit'
+document.getElementById("h").textContent= x ;
+document.getElementById("p").textContent= `excuse me ${y}`;
